31a0d87f54c766abc7c5012e55bd6342cbf3f651
Admin-NLA <sistemas.gruponoria@gmail.com>
2026-01-21 17:34:42 -0600

    Fix: Ajuste useAgenda y Agenda

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext.jsx";
-//import API from "../services/api"; -------- Sin uso
+import API from "../services/api";
 import {
   Calendar,
   Clock,
