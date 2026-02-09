62191a00d0941ecfb0f2e07fddd614f92b877b00
Admin-NLA <sistemas.gruponoria@gmail.com>
2026-02-09 16:49:40 -0600

    Ajustes vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react({ include: /\.(js|jsx)$/ })],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_NOTIF_URL': JSON.stringify(process.env.VITE_NOTIF_URL),
+  },
   server: {
     port: 3000,
     open: true
