30b6a7b3af901d6af11bb006429e6649735bb16a
Admin-NLA <sistemas.gruponoria@gmail.com>
2026-02-11 14:20:29 -0600

    fix: add define config to inject environment variables

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react({ include: /\.(js|jsx)$/ })],
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://cmc-app.onrender.com/api'),
-    'import.meta.env.VITE_NOTIF_URL': JSON.stringify(process.env.VITE_NOTIF_URL || 'https://cmc-app.onrender.com/events'),
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://cmc-app.onrender.com/api'),
+    'import.meta.env.VITE_NOTIF_URL': JSON.stringify('https://cmc-app.onrender.com/events'),
   },
   server: {
     port: 3000,
